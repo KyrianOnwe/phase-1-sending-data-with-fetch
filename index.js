96bb37ba90b07a3497467d3f4ab12e18f15138b3
Kyrian Onwe <dr.kyrian.onwe@gmail.com>
2022-01-02 02:23:53 -0500

    commit tree 3fc06d2ef771b39652e01e83bd5367e8b3e00bae

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+function submitData(name, email){
+    const formData = {
+        "name" : `${name}`,
+        "email" : `${email}`,
+      };
+
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(object){
+            console.log(object.id);
+            let card = document.createElement('li')
+            card.className = 'card'
+            card.innerHTML = `div class="content"><h4>${object.id}</h4></div>`
+
+            document.querySelector("body").appendChild(card)
+        
+        })
+        .catch(function (error) {
+            let card = document.createElement('li')
+            card.className = 'card'
+            card.innerHTML = `div class="content"><h4>${error.message}</h4></div>`
+
+            document.querySelector("body").appendChild(card)
+        }
+      
+        
+
+        )
+
+        }
